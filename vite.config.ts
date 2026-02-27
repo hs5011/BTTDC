0a1c9bae7d345362b59949bc6c5299ebde6e89ca
SonDH <hs5011@gmail.com>
2026-02-27 09:03:30 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './', // Đảm bảo chạy đúng trên GitHub Pages dù là thư mục con
+      build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        emptyOutDir: true,
+      },
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
